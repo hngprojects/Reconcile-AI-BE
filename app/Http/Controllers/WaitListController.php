d4aa0227707a37afe575e02704af3cc18d2a7984
Dev-Tonia <ogunwaanthonia@gmail.com>
2025-03-04 22:17:14 +0100

    fix: fixed the failing ci test

--- app/Http/Controllers/WaitListController.php
+++ app/Http/Controllers/WaitListController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Waitlist;
+use App\Models\WaitList;
 
 class WaitListController extends Controller
 {
@@ -34,7 +34,7 @@ class WaitListController extends Controller
         }
 
         // Save to database
-        $waitlist = new Waitlist();
+        $waitlist = new WaitList();
         $waitlist->email = $request->email;
         $waitlist->save();
 
