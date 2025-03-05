2aa2c92053174595a1481f9cb85276d4c6f43d7f
Idehen Divine <idehendivine16@gmail.com>
2025-03-05 10:32:15 +0100

    feat(auth): added signin feature
    init auth sevice
    added signin method

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -2,10 +2,23 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\AuthLogin;
+use App\Services\Auth\AuthService;
+use Illuminate\Http\JsonResponse;
 
 class AuthController extends Controller
 {
-    //
+    private AuthService $authService;
+
+    public function __construct(AuthService $authService)
+    {
+        $this->authService = $authService;
+    }
+
+    public function login(AuthLogin $request) : JsonResponse
+    {
+        return $this->authService->login($request)->toJson();
+    }
 }
