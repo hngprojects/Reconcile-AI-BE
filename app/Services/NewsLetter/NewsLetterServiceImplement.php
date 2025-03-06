9d19fa579c759b4b34778c0e2bf6c6499f8bb68f
Idehen Divine <idehendivine16@gmail.com>
2025-03-06 09:54:08 +0100

    feat: added newsletter service
    handles business logic for both subscribe and unsubscribe

---
+++ app/Services/NewsLetter/NewsLetterServiceImplement.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Services\NewsLetter;
+
+use LaravelEasyRepository\ServiceApi;
+use App\Repositories\NewsLetter\NewsLetterRepository;
+use Exception;
+
+class NewsLetterServiceImplement extends ServiceApi implements NewsLetterService
+{
+    /**
+     * don't change $this->mainRepository variable name
+     * because used in extends service class
+     */
+    protected NewsLetterRepository $mainRepository;
+
+    public function __construct(NewsLetterRepository $mainRepository)
+    {
+        $this->mainRepository = $mainRepository;
+    }
+
+    public function subscribe($request)
+    {
+        try {
+            $validated = $request->validated();
+            $subscribe = $this->mainRepository->subscribe($validated['email']);
+
+            return $this->setCode(200)
+                ->setMessage("Subscription Successful")
+                ->setData([
+                    'email' => $subscribe['email'],
+                ]);
+        } catch (Exception $e) {
+            return $this->setCode(400)
+                ->setMessage("Subscription Failed")
+                ->setError($e->getMessage());
+        }
+    }
+
+    public function unsubscribe($request)
+    {
+        try {
+            $validated = $request->validated();
+            $unsubscribe = $this->mainRepository->unsubscribe($validated['email']);
+
+            return $this->setCode(200)
+                ->setMessage("Unsubscribed Successfully")
+                ->setData([
+                    'email' => $validated['email'],
+                ]);
+        } catch (Exception $e) {
+            return $this->setCode(400)
+                ->setMessage("Unsubscription Failed")
+                ->setError($e->getMessage());
+        }
+    }
+}
