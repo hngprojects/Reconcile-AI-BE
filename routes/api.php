341d4620bee27cc26679a516a9c2e16f04193392
Dev-Tonia <ogunwaanthonia@gmail.com>
2025-03-09 02:02:15 +0100

    feat: implemented google auth

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\WaitListController;
 use App\Http\Controllers\Api\NewsLetterController;
+use App\Http\Controllers\GoogleAuthController;
 use App\Http\Controllers\ReconciliationController;
 
 Route::prefix('v1')->group(function () {
@@ -16,11 +17,11 @@ Route::prefix('v1')->group(function () {
     Route::prefix('auth')->middleware('guest')->name('auth.')->group(function () {
         Route::post('/login', [AuthController::class, 'login'])->name('login');
         Route::post('/register', [AuthController::class, 'register'])->name('register');
-        // Route::post('/forgot-password', [AuthController::class, 'forgotPassword'])->name('forgot-password');
-        // Route::post('/reset-password', [AuthController::class, 'resetPassword'])->name('reset-password');
-        // Route::post('/verify-email', [AuthController::class, 'verifyEmail'])->name('verify-email');
-        // Route::post('/resend-verification-email', [AuthController::class, 'resendVerificationEmail'])->name('resend-verification-email');
         Route::post('/logout', [AuthController::class, 'logout'])->withoutMiddleware('guest')->middleware('jwt.auth')->name('logout');
+
+        // google auth
+        Route::get('/google', [GoogleAuthController::class, 'redirectToGoogle']);
+        Route::get('/google/callback', [GoogleAuthController::class, 'handleGoogleCallback']);
     });
 
     Route::prefix('newsletter')->name('newsletter.')->group(function () {
@@ -29,6 +30,6 @@ Route::prefix('v1')->group(function () {
     });
 
     Route::post('/reconcile', [ReconciliationController::class, 'reconcile']);
-    Route::post('/wait-list', [WaitListController::class, 'store']);
-    Route::post('/contact', [ContactController::class, 'contact']);
+    Route::post('/wait-list', [WaitListController::class, 'store']); //wait list route
+    Route::post('/contact', [ContactController::class, 'contact']); //contact us route
 });
