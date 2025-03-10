a71374cb9d7479b57c9a23317f26364b1b72d984
cyrilmano <thankgoduchecyril@gmail.com>
2025-03-10 20:14:32 +0100

    fix: update file validation to accept only CSV format in ReconciliationController

--- routes/api.php
+++ routes/api.php
@@ -36,7 +36,7 @@ Route::prefix('v1')->group(function () {
         Route::post('/unsubscribe', [NewsLetterController::class, 'unsubscribe'])->name('unsubscribe');
     });
 
-    Route::post('/reconcile', [ReconciliationController::class, 'reconcile'])->name('reconcile')->middleware(ThrottleUnauthenticated::class);
+    Route::post('/reconcile', [ReconciliationController::class, 'reconcile'])->name('reconcile'); //->middleware(ThrottleUnauthenticated::class);
     Route::post('/reconcile/export', [ReconciliationController::class, 'export'])->name('export');
     Route::post('/wait-list', [WaitListController::class, 'store'])->name('wait-list');
     Route::post('/contact', [ContactController::class, 'contact'])->name('contact');
