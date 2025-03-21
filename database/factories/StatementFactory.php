d0588663a248465c7070d98161339f926e30b2ba
Mitch <ndianguimichelle@gmail.com>
2025-03-21 18:33:24 +0300

    test: updates

--- database/factories/StatementFactory.php
+++ database/factories/StatementFactory.php
@@ -20,7 +20,7 @@ class StatementFactory extends Factory
             'date' => fake()->date(),
             'person' => fake()->name(),
             'other_information' => fake()->sentence(),
-            'amount' => fake()->randomNumber()
-        ];
+            'amount' => fake()->randomNumber(),
+            'reconciliation_id' => \App\Models\Reconciliation::factory()        ];
     }
 }
