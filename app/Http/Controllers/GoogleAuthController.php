b1a7e4e3aa7da5d96851575db59a5f76fce0a7b0
incrisz <incrisz4luv@gmail.com>
2025-03-26 10:30:53 +0100

    updated GoogleAuth

--- app/Http/Controllers/GoogleAuthController.php
+++ app/Http/Controllers/GoogleAuthController.php
@@ -401,26 +401,6 @@ class GoogleAuthController extends Controller
      *     )
      * )
      */
-    /* public function fetchUser(Request $request)
-    {
-        // $user = $request->user();
-        $user = $request->user()->load('paymentPlan.plan');
-        // dd($user);
-        $plan = $user->paymentPlan;
-        // $userPlan = $plan->plan();
-
-        return response()->json([
-            'status_code' => 200,
-            'status' => 'success',
-            'message' => 'User successfully fetched',
-            'data' => [
-                'user' => $request->user(),
-                'plan' => $plan ?? null,
-                'userPlan' => $plan ? $plan->plan : null
-            ]
-        ]);
-    } */
-
     public function fetchUser(Request $request)
     {
         $user = $request->user()->load('paymentPlan.plan');
