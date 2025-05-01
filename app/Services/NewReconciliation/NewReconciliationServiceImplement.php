12c226b616a3e0fca410f6e118c82f1e6ca8ddfc
Mitch <ndianguimichelle@gmail.com>
2025-05-01 15:56:04 +0300

    fix: compilation

--- app/Services/NewReconciliation/NewReconciliationServiceImplement.php
+++ app/Services/NewReconciliation/NewReconciliationServiceImplement.php
@@ -722,7 +722,7 @@ class NewReconciliationServiceImplement extends ServiceApi implements NewReconci
             $reconciliations = $this->mainRepository->list($user);
             $statements = [];
             foreach ($reconciliations as $recon) {
-                $reconciliation = $this->mainRepository->get($recon->id);
+                $reconciliation = $this->mainRepository->get($recon['id']);
                 $statements[] = $this->statementRepository->findAll($reconciliation);
             }
 
